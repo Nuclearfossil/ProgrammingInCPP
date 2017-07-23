45a9e4c7884814cec4ddff509c8a6b2f8a2c7f4d
Ash Matheson <nuclearfossil@hotmail.com>
2017-07-23 07:22:12 -0700

    removing build folder

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{guid:[],index:0,modul:0,page:0,search:0},titles:["Programming in C++ Review"],titleterms:{document:[],guid:0,indic:0,program:0,programminginc:[],review:0,tabl:0,welcom:[]}})
