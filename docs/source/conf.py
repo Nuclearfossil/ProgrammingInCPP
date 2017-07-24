9bc84afc62b172b1d54b190c5a58f1c6047ed1b6
Ash Matheson <nuclearfossil@hotmail.com>
2017-07-23 22:37:48 -0700

    back to normal

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import cloud_sptheme as csp
+
 
 # -- General configuration ------------------------------------------------
 
@@ -85,10 +85,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'cloud'
-html_theme_path = [csp.get_theme_dir()]
-html_theme_options = { "roottarget": "index" }
+html_theme = 'sphinx_rtd_theme'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
